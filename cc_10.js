6b864bee592e4aaa28664829bb4516f9f0c76cb8
Jbarb25 <barboza205@usf.edu>
2025-02-20 12:36:04 -0500

    Task 4 - Implemented Order Management
    Create a new method placeOrder to place the new. This is done by finding and ensuring that the product does exist through its ID and then uses an if else statement to ensure that the order can be placed by seeing if there is enough quantity to cover what's requested. This also calculates the total price of the order and then adds the order information to the order array  and logs the order details to the console.

--- cc_10.js
+++ cc_10.js
@@ -12,7 +12,6 @@ class Product {    //Create a class 'product" with the properties below
     updateStock(quantity){   //Add a method that modifies the stock level when an order is placed to remove the stock ordered 
         this.stock -= quantity;
         return(this.stock);} //Return the updated stock level to where it is called
-
 }
 const prod1 = new Product("Laptop", 101, 1200, 10); //establish a new product with the parameters to plug into the functions
 console.log(prod1.getDetails()); 
@@ -41,15 +40,40 @@ console.log(prod1.getDetails()); //call the get Details method from the Product 
 //Task 3: Creating an Inventory Class
 class Inventory{   //create and Inventory class that has the product as a parameter
     constructor(products){
-        this.products = [];} //establish products as an array of products for the inventory
-    
+        this.products = []; //establish products as an array of products for the inventory
+        //Task 4: Implementing Order Management
+        this.orders = [];}
+
     addProduct(product){  
         this.products.push(product);} //add a product to the array of products using the push action
     
     listProducts(){
     this.products.forEach(product =>{  //for each product in the array of products get the details of the product and print to the console
     console.log(product.getDetails())});}
+
+    //Task 4: 
+    placeOrder(orderId, product, quantity){
+        const inventoryProduct = this.products.find(p => p.id === product.id); //inventory product is assigned to the product and it's id located in the products array to ensure it exists
+        if (inventoryProduct && inventoryProduct.stock >= quantity){  //This ensures that the product is a valid product and that stock has enough to cover the quantity ordered 
+            const totalPrice = product.price*quantity  //This would calculate the total cost of the order 
+            const order = new Order(orderId, product, quantity); //encapsulates a new order information in this method 
+            this.orders.push(order); //adds the order information into the array of orders
+            console.log(`Order ${orderId} Placed, Total Cost: $${totalPrice}`);} //logs that there is a new order placed 
+        else{
+            console.log(`Not Enough Stock`);}} //if there is not enough stock to cover the qty in the order, the message will appear
+    
+    listOrders(){
+        this.orders.forEach(order => {   //for each order in the order array, list the details of the order 
+            console.log(order.getOrderDetails());})
+    }
 }
+
 const inventory = new Inventory();  //initiate a new inventory 
 inventory.addProduct(prod1);  //add product1 to the inventory class and run it through the methods to add it to the array of products
 inventory.listProducts();  //call the method list products to list the products' information from the array
+
+
+//Task 4: Implementing Order Management
+inventory.placeOrder(601, prod1, 2);  //establish a new order to place
+inventory.listOrders();  //call the method to list the details of the order after it is placed
+console.log(prod1.getDetails());  //call the method to list the details of the product ordered
