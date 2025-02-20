5dcf797010ab5a82221b644ff65973a209bfcf54
Jbarb25 <barboza205@usf.edu>
2025-02-19 21:11:41 -0500

    Task 2 - Created Order Class
    Created an Order class that has the order id, product, and quantity properties. Added a method that refers back to the updatestock method in the Product class to plug in the new this.quantity from the order into the quantity from the product. Added another method to return the details of the order when called.

--- cc_10.js
+++ cc_10.js
@@ -9,9 +9,9 @@ class Product {    //Create a class 'product" with the properties below
     getDetails(){ //Add a method that returns a formatted string of product details when called with the parameters to plug in
         return(`Product Name: ${this.name}, ID: ${this.id}, Price:$${this.price}, Stock: ${this.stock}`);}
 
-    updateStock(quanity){   //Add a method that modifies the stock level when an order is placed to remove the stock ordered 
-        this.stock -= quanity;
-        return(`Updated Stock Level: ${this.stock}`);} //Return the updated stock level to where it is called
+    updateStock(quantity){   //Add a method that modifies the stock level when an order is placed to remove the stock ordered 
+        this.stock -= quantity;
+        return(this.stock);} //Return the updated stock level to where it is called
 
 }
 const prod1 = new Product("Laptop", 101, 1200, 10); //establish a new product with the parameters to plug into the functions
@@ -19,3 +19,20 @@ console.log(prod1.getDetails());
 
 prod1.updateStock(3); //plug in the 3 stock ordered into the updatestock method to remove the order from the stock level and return the updated level
 console.log(prod1.getDetails()); //return the updated product details to the console
+
+//Task 2: Creating an Order Class
+class Order {   //Create an Order class that has the properties below
+    constructor(orderID, product, quantity){
+        this.orderID = orderID;
+        this.product = product;
+        this.quantity = quantity;
+
+        this.product.updateStock(this.quantity);} //Update the stock level by calling the method from the Product class and inserting this.quantity for quantity
+
+    getOrderDetails(){ //return the Order details with their labels when called. this.product is an instance of Product so now it is pulled from Product class
+        return (`Order ID: ${this.orderID}, Product: ${this.product instanceof Product}, Quantity: ${this.quantity}`);} 
+}
+
+const order1 = new Order(501, prod1, 2); //Initiate a new order with the parameter for details
+console.log(order1.getOrderDetails()); //call the Order Details method in the class to print out the details of the order
+console.log(prod1.getDetails()); //call the get Details method from the Product class to print out the updated details of the Product
